c10f136a69ee52fd021eb1dbe77c3fc28ad2a74f
fainapahanko <f.poganko@gmail.com>
2020-01-31 12:01:14 +0100

    search with redux implemented

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ export default function (state = {}, action) {
     switch(action.type) {
         case "SEARCH_ARTIST": 
             return {
-                ...state,
                 searchArtist: action.payload
             }
         default:
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from 'redux-thunk'
-import rootReducer from "../reducers";
+import rootReducer from "../reducers/index";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const initialState = {
-    searchArtist: ""
+    searchArtist: undefined
 }
 
 export default function configureStore() {
